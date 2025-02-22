523d385bdbf51031d82fab8cc40d31189e10f198
WANI-OWAIS <waniowais528@gmail.com>
2025-02-22 18:24:29 +0530

    step1

---
+++ Step1-Basics/p1.cpp
@@ -0,0 +1,142 @@
+// #include<iostream>
+#include <bits/stdc++.h> // This is a header file which includes all the header files
+using namespace std;
+
+int main()
+{
+    // cout<<"Hey Striver !" <<endl;
+
+    // int x,y;
+    // cin >> x >> y;
+    // cout << "value of x: " << x << " and y: " << y;
+
+    // Data types
+    //  int x = 10;
+    //  long x = 10;
+    //  long long x = 1000000000000000000;
+    //  float x = 10.10;
+    //  double x = 10.10;
+    //  char x = 'A';
+    // string
+    // string x;
+    // string y;
+    // cin >> x >> y;
+    // cout << x << " " << y;
+    // getline
+    // string str;
+    // getline(cin, str);
+    // cout << str;
+
+    // if else statement
+    // write a program that takes an input of age and prints if you are adult or not
+    //  int age;
+    //  cin >> age;
+    //  if(age>=18){
+    //      cout << "You are an adult" << endl;
+    //  }
+    //  else{
+    //      cout << "You are not an adult" << endl;
+    //  }
+
+    // switch statement
+    // Take the day no as input and print the corresponding day for 1 print Monday, for 2 print Tuesday and so on for 7 print Sunday
+    //  int day;
+    //  cin >> day;
+    //  switch(day){
+    //      case 1:
+    //          cout << "Monday" << endl;
+    //          break;
+    //      case 2:
+    //          cout << "Tuesday" << endl;
+    //          break;
+    //      case 3:
+    //          cout << "Wednesday" << endl;
+    //          break;
+    //      case 4:
+    //          cout << "Thursday" << endl;
+    //          break;
+    //      case 5:
+    //          cout << "Friday" << endl;
+    //          break;
+    //      case 6:
+    //          cout << "Saturday" << endl;
+    //          break;
+    //      case 7:
+    //          cout << "Sunday" << endl;
+    //          break;
+    //      default:
+    //          cout << "Invalid day" << endl;
+
+    // }
+
+    // Basics Array,Strings
+    // 1D Array
+    //   int arr[5] = {1,2,3,4,5};
+    //   cout << arr[0] << endl;
+
+    // 2D Array
+    //  int arr[2][3] = {{1,2,3},{4,5,6}};
+    //  cout << arr[0][1] << endl;
+
+    // Strings
+    //  string s = "Striver";
+    //  int len = s.size();
+    //  s[len -1 ] = 'z';
+    //  cout << s[len - 1];
+
+    // loops
+    // for loop
+    // for(int i=0; i<5; i++){
+    //     cout << "Striver" << endl;
+    // }
+
+    // while loop
+    //  int i=0;
+    //  while (i<5)
+    //  {
+    //      cout << "Striver" << endl;
+
+    //     i++;
+    // }
+
+    // do while
+    //  int i=0;
+    //  do
+    //  {
+    //      cout << "Striver" << endl;
+    //      i++;
+    //  } while (i<5);
+
+    // functions -- are used to use same code multiple times
+
+    // void fun -- which does not return anything
+    // void printName()
+    // {
+    //     cout << "hey striver";
+    // }
+
+    // printName();
+
+    //parametrised
+    // void printName(string name) {
+    //     cout << "hey" << name;
+    // }
+
+    // string name;
+    // cin >> name;
+    // printName(name);
+
+    //return
+    // int sum(int num1, int num2){
+    //     int num3 = num1 + num2;
+    //     return num3;
+    // }
+
+
+    // int num1, num2;
+    // cin >> num1 >> num2;
+    // int res = sum(num1, num2);
+    // cout << res;
+
+    return 0;
+}
